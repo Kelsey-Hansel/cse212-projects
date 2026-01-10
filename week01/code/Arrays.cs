65617f02a96f5763ce7d4b2dff7f0fdafaa1d9b3
Kelsey-Hansel <kelseylhansel@gmail.com>
2026-01-10 16:58:40 -0600

    Week 1 code work for homework and learning activities.

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,22 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // Kelsey's code process: 
+        // variable to store results
+        // variable to store each multiple one at a time, starts with "number"
+        // for loop that iterates "length" times
+        // in each iteration, multiple variable is added to the results variable as a multiple of "number"
+        // add "number" to multiple variable to get next multiple
+
+        var results = new double[length];
+        double multiple = number;
+        for (int i = 0; i < length; i++)
+        {
+            results[i] = multiple;
+            multiple += number;
+        }
+
+        return results; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +44,14 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Kelsey's code process:
+        // Use GetRange() to get the list values that will be rotated stored in a varaible
+        // Use InsertRange() to put the rotated values to their final rotated position at the start of the list
+        // Use RemoveRange() to remove the dupicated rotated values from the end of the list
+
+        var rotateRange = data.GetRange(data.Count - amount, amount);
+        data.InsertRange(0, rotateRange);
+        data.RemoveRange(data.Count - amount, amount);
     }
 }
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,22 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var results = new int[select.Length];
+        int l1Index = 0;
+        int l2Index = 0;
+        for (int i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+            {
+                results[i] = list1[l1Index];
+                l1Index++;
+            }
+            else if (select[i] == 2)
+            {
+                results[i] = list2[l2Index];
+                l2Index++;
+            }
+        }
+        return results;
     }
 }
